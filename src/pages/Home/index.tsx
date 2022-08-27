039ec2971f16a1b55c2e1bf1433c653a6696c51e
Lucas Estevam <dev.lucasestevam@gmail.com>
2022-08-27 12:18:59 -0300

    feat: inicial project structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './styles';
+
+export const Home = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome Lucas</Text>
+    </View>
+  );
+};
